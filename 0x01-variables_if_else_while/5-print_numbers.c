e299d2d7405b760a387f155e93eb33f173705c7c
aatogebania alexander <aatogebania@gmail.com>
2023-02-17 16:49:41 -0800

    6 done

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print all digit numbers of base 10 from 0
+ *  
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char ch = '1';
+
+	while (ch <= '9')
+	{
+		putchar(ch);
+		++ch;
+	}
+	putchar('\n');
+
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints all single digit numbers of base 10 from 0
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
