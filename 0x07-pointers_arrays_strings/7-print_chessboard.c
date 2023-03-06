83561e91e7ad043c8c01a34f2a7ff8cf468efe12
aatogebania alexander <aatogebania@gmail.com>
2023-03-06 15:16:24 -0800

    all done

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,3 +1,4 @@
+#include "main.h"
 /**
  * print_chessboard - prints the chess board
  *
--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 /**
  * print_diagsums - print the sum of the two
  *                 diagonals of a square matrix
