ce7db143bb43a192b5da8a9d65a64cd226c72e5b
aatogebania alexander <aatogebania@gmail.com>
2023-03-20 17:58:49 -0700

    all done

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,6 @@
+#ifndef OBJECT_LIKE_MACRO_
+#define OBJECT_LIKE_MACRO_
+
+#define SIZE 1024
+
+#endif
---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * Return: 0 if success
+ */
+
+int main(void)
+{
+	printf(__FILE__);
+	printf("\n");
+	return (0);
+}
